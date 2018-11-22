c803704941518945b18d72f35f230a19f7c46ae9
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2018-11-22 14:12:13 +1100

    Template for function

---
+++ R/genderCode.R
@@ -0,0 +1,20 @@
+#' Gender column recoder
+#'
+#' Recodes gender column to
+#'
+#' @param input vector of gender
+#' @param character expansion for the text
+#' @param mar margin parameters; vector of length 4 (see \code{\link[graphics]{par}})
+#' @param
+#'
+#'
+#'
+#' @return None
+#'
+#' @examples
+#' plot_crayons()
+#'
+#' @export
+genderRecode <-
+  function()
+  { ... }
