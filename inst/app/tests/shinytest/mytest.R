fb18290ad00205e8d580ab87f2007d97cad9ba6a
Lingtax <m.ling@deakin.edu.au>
2021-05-24 18:22:30 +1000

    update app tests for comprehensiveness, integrates with testthat

--- inst/app/tests/shinytest/mytest.R
+++
@@ -1,9 +0,0 @@
-app <- ShinyDriver$new("../../")
-app$snapshotInit("mytest")
-
-app$setInputs(dictionary = "narrow")
-app$setInputs(dlformat = ".sav")
-app$setInputs(dlformat = ".dta")
-app$setInputs(sidebarCollapsed = TRUE)
-app$setInputs(sidebarCollapsed = FALSE)
-app$snapshot()
---
+++ tests/testthat/test-app.R
@@ -0,0 +1,15 @@
+context("app")
+# This file is for testing the applications in the inst/ directory.
+
+library(shinytest)
+
+test_that("app works", {
+  # Don't run these tests on the CRAN build servers
+  skip_on_cran()
+
+  # Use compareImages=FALSE because the expected image screenshots were created
+  # on a Mac, and they will differ from screenshots taken on the CI platform,
+  # which runs on Linux.
+  appdir <- system.file(package = "gendercoder", "app")
+  expect_pass(testApp(appdir, compareImages = FALSE))
+})
